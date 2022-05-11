4db2b169f1c94b3d1e7568c871c4868c54844db8
YamaOk <o1.d00m.o6@gmail.com>
2022-05-11 14:11:07 +0900

    Designer画面【レスポンシブ対応】

--- assets/js/schedule-handler.js
+++ assets/js/schedule-handler.js
@@ -1,9 +1,10 @@
 const playScheduleAnimation = (schedule, time) => {
-    const isOpened = schedule.classList.contains("schedule-opened")
-    schedule.classList.remove(isOpened ? "schedule-opened" : "schedule-closed")
+    const parent = schedule.parentElement
+    const isOpened = parent.classList.contains("schedule-opened")
+    parent.classList.remove(isOpened ? "schedule-opened" : "schedule-closed")
     window.requestAnimationFrame((time) => {
         window.requestAnimationFrame((time) => {
-            schedule.classList.add( isOpened ? "schedule-closed" : "schedule-opened")
+            parent.classList.add( isOpened ? "schedule-closed" : "schedule-opened")
         })
     })
 }

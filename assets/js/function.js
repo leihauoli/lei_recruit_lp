adcb2b4fa6013f3456836f0961abe224b9225885
mitsuki369 <k.inada@leihauoli.com>
2022-05-31 13:41:41 +0900

    FB修正

--- assets/js/function.js
+++ assets/js/function.js
@@ -5,60 +5,60 @@ LEIHAUOLI.RECRUIT_DESIGNER = LEIHAUOLI.RECRUIT_DESIGNER || {};
  * 画面をスクロールして該当要素が表示領域に入ったとき、該当要素をアニメーションさせる
  */
 LEIHAUOLI.RECRUIT_DESIGNER.AnimateByScroll = {
-    init: function(){
-        this.setParameters();
-        this.bindEvent();
-    },
-    setParameters: function(){
-      this.$window = $(window);
+  init: function(){
+    this.setParameters();
+    this.bindEvent();
+  },
+  setParameters: function(){
+    this.$window = $(window);
+    if (window.matchMedia('(max-width: 1024px)').matches){
+      this.headerOptions = {
+        rootMargin: '-60px 0px 0px 0px'
+      };
+    } else {
+      this.headerOptions = {
+        rootMargin: '-76px 0px 0px 0px'
+      };
+    }
+    this.$header = $('.jsc-main-header');
+    this.$sectionKv = $('.jsc-section-kv');
+    this.headerObserver = new IntersectionObserver(this.changeHeaderBg, this.headerOptions);
+  },
+  bindEvent: function(){
+    var myself = this;
+    Array.prototype.forEach.call(this.$sectionKv, function(box){
+      myself.headerObserver.observe(box);
+    });
+    this.$window.on('resize', function(){
       if (window.matchMedia('(max-width: 1024px)').matches){
-        this.headerOptions = {
+        myself.headerOptions = {
           rootMargin: '-60px 0px 0px 0px'
         };
       } else {
-        this.headerOptions = {
+        myself.headerOptions = {
           rootMargin: '-76px 0px 0px 0px'
         };
       }
-      this.$header = $('.jsc-main-header');
-      this.$sectionKv = $('.jsc-section-kv');
-      this.headerObserver = new IntersectionObserver(this.changeHeaderBg, this.headerOptions);
-    },
-    bindEvent: function(){
-      var myself = this;
-      Array.prototype.forEach.call(this.$sectionKv, function(box){
-        myself.headerObserver.observe(box);
-      });
-      this.$window.on('resize', function(){
-        if (window.matchMedia('(max-width: 1024px)').matches){
-          myself.headerOptions = {
-            rootMargin: '-60px 0px 0px 0px'
-          };
-        } else {
-          myself.headerOptions = {
-            rootMargin: '-76px 0px 0px 0px'
-          };
+    });
+  },
+  changeHeaderBg: function(entries){
+    Array.prototype.some.call(entries, function(entry){
+      if (!entry.isIntersecting){
+        if (!$('.jsc-main-header').is(':animated')){
+          $('.jsc-main-header').removeClass('is-hidden').addClass('is-change');
+          $('.jsc-header-logo').attr('src', '/assets/images/header-black-logo.png');
         }
-      });
-    },
-    changeHeaderBg: function(entries){
-      Array.prototype.some.call(entries, function(entry){
-        if (!entry.isIntersecting){
-          if (!$('.jsc-main-header').is(':animated')){
-            $('.jsc-main-header').removeClass('is-hidden').addClass('is-change');
-            $('.jsc-header-logo').attr('src', '/assets/images/header-black-logo.png');
+      } else {
+        if (!$('.jsc-main-header').is(':animated')){
+          if ($('.jsc-main-header').css('position') === 'fixed'){
+            $('.jsc-main-header').addClass('is-hidden');
           }
-        } else {
-          if (!$('.jsc-main-header').is(':animated')){
-            if ($('.jsc-main-header').css('position') === 'fixed'){
-              $('.jsc-main-header').addClass('is-hidden');
-            }
-            $('.jsc-main-header').removeClass('is-change');
-            $('.jsc-header-logo').attr('src', '/assets/images/header-logo.png');
-          }
+          $('.jsc-main-header').removeClass('is-change');
+          $('.jsc-header-logo').attr('src', '/assets/images/header-logo.png');
         }
-      });
-    }
+      }
+    });
+  }
 };
 
 
@@ -79,12 +79,8 @@ LEIHAUOLI.RECRUIT_DESIGNER.AnimateByScroll = {
     this.$headerMenuList = $('.jsc-header-menu-list');
     this.$headerMenuItem = this.$headerMenuList.find('li').find('a');
     this.$hamburgerMenu = $('.jsc-hamburger-menu');
-    this.$headerLogoList = $('.jsc-header-logo-list');
     this.$headerNavSp = $('.jsc-header-nav-sp');
     this.$hamburgerBg = $('.jsc-hamburger-bg');
-    this.$hamburgerBorderTop = $('.jsc-hamburger-border-top');
-    this.$hamburgerBorderMiddle = $('.jsc-hamburger-border-middle');
-    this.$hamburgerBorderBottom = $('.jsc-hamburger-border-bottom');
   },
   bindEvent: function(){
     var myself = this;
@@ -99,11 +95,9 @@ LEIHAUOLI.RECRUIT_DESIGNER.AnimateByScroll = {
     this.$hamburgerBg.on('click', this.toggleHamburger.bind(this));
   },
   toggleHamburger: function(){
-    this.$hamburgerMenu.toggleClass(this.ACTIVE_CLASS);
     this.$body.toggleClass(this.FIXED_CLASS);
-    this.$headerMenuList.toggleClass(this.ACTIVE_CLASS);
+    this.$hamburgerMenu.toggleClass(this.ACTIVE_CLASS);
     this.$headerNavSp.toggleClass(this.ACTIVE_CLASS);
-    this.$hamburgerBg.toggleClass(this.ACTIVE_CLASS);
   }
 };
 
